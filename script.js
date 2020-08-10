7d6df45e1fb9eae0183e882b39981c42e11f2cfd
hm-Jeon <69206253+hm-Jeon@users.noreply.github.com>
2020-08-11 01:47:41 +0900

    4th version

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function setBodyColor(bg, fg) {
-  var target = document.querySelector('body');
-  target.style.backgroundColor=bg;
-  target.style.color=fg;
-  target.style.borderColor=fg;
+var body = {
+  setColor: function(color) {
+    var target = document.querySelector('body');
+    target.style.color=color;
+    target.style.borderColor=color;
+  },
+  setBackgroundColor: function(color) {
+    var target = document.querySelector('body');
+    target.style.backgroundColor=color;
+  }
 }
 
-function setLinkColor(color) {
-  var alist = document.querySelectorAll('a');
-  var i = 0;
-  while(i < alist.length){
-      alist[i].style.color = color;
-      i++;
+var links = {
+  setColor: function (color) {
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length){
+        alist[i].style.color = color;
+        i++;
+    }
   }
 }
 
@@ -23,13 +30,15 @@ function syncValue(mode) {
 
 function nightDayHandler(self) {
   if(self.value == '다크 모드') {
-    setBodyColor('black', 'white');
-    setLinkColor('white');
+    body.setColor('white');
+    body.setBackgroundColor('black');
+    links.setColor('white');
     syncValue('기본 모드');
   }
   else {  // self.value == '기본모드'
-    setBodyColor('white', 'black');
-    setLinkColor('black');
+    body.setColor('black');
+    body.setBackgroundColor('white');
+    links.setColor('black');
     syncValue('다크 모드');
   }
 }
